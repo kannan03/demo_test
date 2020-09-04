233b87f17be70d86fb569953d0e80c5b2e210c1e
kannan <sivakannan3139@gmail.com>
2020-09-04 10:07:32 +0530

    add chg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+    @ViewChild('agGrid') agGrid: AgGridAngular;
+
+    title = 'app';
+
+    columnDefs = [
+      {headerName: 'ThumbnailUrl', field: 'thumbnailUrl', sortable: true, filter: true},
+      {headerName: 'AlbumId', field: 'albumId', sortable: true, filter: true},
+      {headerName: 'ID', field: 'id', sortable: true, filter: true},
+      {headerName: 'Title', field: 'title', sortable: true, filter: true},
+      {headerName: 'Url', field: 'url', sortable: true, filter: true}
+  ];
+    rowData: any;
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit() {
+        this.rowData = this.http.get('https://jsonplaceholder.typicode.com/photos');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AgGridModule.withComponents([]),
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path :'login', component:AppComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
